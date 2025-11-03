5be149d7db4a188b50bc7495c465ab049e76f411
Santiagocapi <s.capitani@alumno.um.edu.ar>
2025-11-02 23:23:47 -0300

    feat: Implement GET /api/playlist/my to get playlist

--- backend/routes/playlistRoutes.js
+++ backend/routes/playlistRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
-const { createPlaylist } = require('../controllers/playlistController');
+const { createPlaylist, getUserPlaylists } = require('../controllers/playlistController');
 
 // ROUTES
 
 // POST /api/playlists - Create a new playlist (Private)
 // PRIVATE
 router.post('/', protect, createPlaylist);
+
+// GET /api/playlists 
+// PRIVATE
+router.get('/my', protect, getUserPlaylists);
 
 module.exports = router;
